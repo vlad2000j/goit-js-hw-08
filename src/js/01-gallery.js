b29cb19d97e9d08e136b8c9990c3c033f4db4860
Житінський Владислав <114184074+vlad2000j@users.noreply.github.com>
2023-03-26 13:46:05 +0300

    1 task done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,31 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryElements = [];
+
+galleryItems.map(galleryItem => {
+	const a = document.createElement("a");
+	a.classList.add("gallery__item");
+	a.href = galleryItem.original;
+
+	const img = document.createElement("img");
+	img.classList.add("gallery__image");
+	img.src = galleryItem.preview;
+	img.alt = galleryItem.description;
+
+	a.append(img);
+
+	galleryElements.push(a);
+});
+
+gallery.append(...galleryElements);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+	captionsData: "alt",
+	captionDelay: 250,
+});
