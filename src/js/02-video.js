a0f0e41ea3451a825c4ac194d2e4a510946f0179
Житінський Владислав <114184074+vlad2000j@users.noreply.github.com>
2023-03-26 15:32:26 +0300

    ls 8

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,33 @@
 
+import throttle from "lodash.throttle";
+import Player from '@vimeo/player';
+
+
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const time = player.on('timeupdate', throttle(function(data) {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(data));
+}, 1000));
+
+
+
+currentTimeFn();
+
+function currentTimeFn(){
+    const array = JSON.parse(localStorage.getItem('videoplayer-current-time'));
+    if(array){
+        const currentTime = array['seconds'];
+        player.setCurrentTime(currentTime);
+    }
+}
+
+
+
+
+
+
+
+
+
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('[name="email"]')
+const textarea = document.querySelector('[name="message"]');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onTextareaInput, 500));
+
+const formData = {};
+
+const STORAGE_KEY = 'feedback-form-state';
+
+function onFormSubmit(e){
+e.preventDefault();
+e.currentTarget.reset();
+localStorage.removeItem(STORAGE_KEY);
+console.log(formData);
+}
+
+function onTextareaInput(e){
+formData.email = input.value;
+formData.message = textarea.value;
+
+localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+}
+
+populateTextarea();
+
+function populateTextarea(){
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+    if(savedData){
+        input.value = savedData.email;
+        textarea.value = savedData.message;
+    }
+}
